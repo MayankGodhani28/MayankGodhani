7327386682898c0b9bc45b57a35fa5905a6f9469
MayankGodhani28 <mayankgodhani222@gmail.com>
2024-02-29 17:52:05 +0530

    its trans

---
+++ matrixtrans.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    int tran[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(i==j)
+            {
+               tran[i][j] = arr[i][j];
+            }else
+            {
+               tran[i][j] = arr[j][i];
+            }
+        }
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cout<<tran[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
